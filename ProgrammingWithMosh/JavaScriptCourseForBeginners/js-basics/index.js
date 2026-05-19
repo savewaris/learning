1ca380a0c020a506830b705a7c2b0ac68c336ab8
savewaris <savewaris@gmail.com>
2026-05-19 08:06:36 +0700

    Learn Variable, Constants, Primitive Typing, Dynamic Typing

--- ProgrammingWithMosh/JavaScriptCourseForBeginners/js-basics/index.js
+++ ProgrammingWithMosh/JavaScriptCourseForBeginners/js-basics/index.js
@@ -1,2 +1,34 @@
 // This is my first JavaScript code
-console.log('Hello World');
+// console.log('Hello World');
+
+// Variable
+// Cannot be a reserved keyword
+// Should be meaningful
+// Cannot start with a number (1name)
+// Cannot contain a space or hyphen (-)
+// Are case-sensitive
+
+// let firstName = 'Save', lastName = 'Waris';
+// let firstName = 'Save';
+// let lastName = 'Waris';
+
+let name = 'Save'; 
+console.log(name);
+
+
+let let_interestRate = 0.3;
+let_interestRate = 1;
+console.log(let_interestRate);
+
+// const const_interestRate = 0.3;
+// const_interestRate = 1;
+// console.log(const_interestRate);
+
+
+let name = 'Save'; // String Literal
+let age = 23; // Number Literal
+let isApproved = false; // Boolean Literal
+let firstName = undefined; 
+let lastName = null; 
+let selectedColor = null; // -> 'red', 'blue', 'green'
+
