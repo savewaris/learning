e1386c172156b92b0005fb2fd9e60f2091b6b884
savewaris <savewaris@gmail.com>
2026-05-19 13:18:47 +0700

    feat: implement login and sign-up pages with Clerk integration

--- FrontendLab/FinanceSaasPlatform/fl-expense-tracker/frontend/app/login/page.tsx
+++ FrontendLab/FinanceSaasPlatform/fl-expense-tracker/frontend/app/login/page.tsx
@@ -1,5 +1,11 @@
-const LoginPage = () => { 
-    return <div>Login</div>
-}
+import { SignIn } from "@clerk/nextjs";
+
+const LoginPage = () => {
+    return (
+        <div className="flex items-center justify-center h-full w-full">
+            <SignIn routing="hash" signUpUrl="/sign-up"/>
+        </div>
+    );
+};
 
 export default LoginPage;
--- FrontendLab/FinanceSaasPlatform/fl-expense-tracker/frontend/app/sign-up/page.tsx
+++ FrontendLab/FinanceSaasPlatform/fl-expense-tracker/frontend/app/sign-up/page.tsx
@@ -4,7 +4,7 @@ import { SignUp } from "@clerk/nextjs";
 const SignUpPage = () => {
     return (
         <div className="flex items-center justify-center h-full w-full">
-            <SignUp routing="hash" />
+            <SignUp routing="hash" signInUrl="/login"/>
         </div>
     );
 };
