05286518014dc3b793d2a566f44a8878f4c9b3ce
Waris Khamkaweepart <savewaris@gmail.com>
2026-05-25 21:01:48 +0700

    Add basic variable examples to helloworld.cpp
    Replace simple pizza printouts with basic variable and datatype examples in helloworld.cpp. Commented out the original std::cout lines and added integer and double variable declarations (age, year, days, price, gpa, temperature) and a sample output (price). The compiled binary (helloworld.exe) was also updated.

--- BroCode/C++FullCourseforfree/C++Programs/helloworld.cpp
+++ BroCode/C++FullCourseforfree/C++Programs/helloworld.cpp
@@ -12,7 +12,36 @@ multi-line
 comment
 */
 
-  std::cout << "I like pizza!" << '\n'; // std::endl
-  std::cout << "It's really good!" << '\n'; // std::endl
+  // std::cout << "I like pizza!" << '\n'; // std::endl
+  // std::cout << "It's really good!" << '\n'; // std::endl
+  // return 0;
+
+  // Variable and basics data type
+
+  // int x; // Declaration
+  // x = 5; // Assignment
+
+  // // int x = 5;
+
+  // int y = 6;
+  // int sum = x + y;
+  // std::cout << x << '\n';
+  // std::cout << y << '\n';
+  // std::cout << sum << '\n';
+
+  // Integer (whole number)
+
+  int age = 23;
+  int year = 2025;
+  int days = 7;
+
+  // int days = 7.5; //Output will be 7 because it can't store .5
+
+  // Double (number including decimal)
+  double price = 10.99;
+  double gpa = 2.5;
+  double temperature = 25.1;
+
+  std::cout << price;
   return 0;
 }
