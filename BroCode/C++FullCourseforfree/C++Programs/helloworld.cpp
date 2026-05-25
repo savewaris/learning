717ce750ccd3ba6570415f023809fcfe920761ee
Waris Khamkaweepart <savewaris@gmail.com>
2026-05-25 19:00:23 +0700

    Add comments to helloworld.cpp
    Insert a single-line comment and a multi-line comment block in main, and add inline comments noting std::endl on the two std::cout lines. No functional changes to program output or logic.

--- BroCode/C++FullCourseforfree/C++Programs/helloworld.cpp
+++ BroCode/C++FullCourseforfree/C++Programs/helloworld.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 
 int main(){
-  std::cout << "I like pizza!" << '\n';
-  std::cout << "It's really good!" << '\n';
+
+// This is a comment
+
+/*
+this
+is
+a
+multi-line
+comment
+*/
+
+  std::cout << "I like pizza!" << '\n'; // std::endl
+  std::cout << "It's really good!" << '\n'; // std::endl
   return 0;
 }
